d33c29043246cdcdad4138c0d69281ac5ffaf309
LorisJunior <em-lourival@outlook.com>
2023-01-06 08:41:57 -0300

    v2.5 Bg script refactored

---
+++ Assets/Scripts/Repeating_Refined/BgLoopRefined.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BgLoopRefined : MonoBehaviour
+{
+    public GameObject background;
+    public Sprite spriteRef;
+    private float spriteSizeX;
+    private Vector3 currentWorldPosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        spriteSizeX = spriteRef.bounds.size.x;
+        currentWorldPosition = background.transform.position;
+    }
+    void Update() 
+    {
+        background.transform.position = currentWorldPosition;
+        float distance = Mathf.Abs(background.transform.localPosition.x);
+
+        if (distance > spriteSizeX)
+        {
+            UpdateBgPosition(distance);
+        }
+    }
+
+    void UpdateBgPosition(float distance)
+    {
+        float offset = distance - spriteSizeX;
+        background.transform.localPosition = new Vector3(transform.position.x > 0 ? -offset : offset, currentWorldPosition.y, currentWorldPosition.z);
+        currentWorldPosition = background.transform.position;
+    }
+}
